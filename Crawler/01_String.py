2a36cce3e2ed0a86d3e3198408c839a6a0a9462d
Woo-Hyun Kim <woosa7@gmail.com>
2016-08-12 22:55:26 +0900

    Stirng, List, Tuple

--- Crawler/01_String.py
+++ Crawler/01_String.py
@@ -1,3 +1,31 @@
-str = "Python for Data Analysis"
-a = str.count("a")
+# 특정 문자 갯수
+str1 = "Python for Data Analysis"
+a = str1.count("a")
 print(a)
+
+
+# 특정 문자 위치
+b1 = str1.index("y")
+b2 = str1.index("y", 5)     # start index
+
+print(b1)
+print(b2)
+
+
+# 좌우 공백 제거
+str1.strip()
+str1.lstrip()
+str1.rstrip()
+
+
+# 문자열 바꾸기
+str1.replace("a", "x")
+str1.replace("a", "x", 2)   # max
+
+
+# 문자열 나누기
+c1 = str1.split(" ")
+c2 = str1.split(" ", 2)     # split count
+
+print(c1)
+print(c2)
---
+++ Crawler/02_List_Tuple.py
@@ -0,0 +1,36 @@
+# List & Tuple
+ddd
+ddd
+
+
+# 리스트에 데이터 추가 삭제
+list1 = [1, "Data Science", 100]
+
+list1.append(9)
+list1.insert(1, "R")
+
+del list1[1]
+list1.remove(100)
+
+
+# 정렬
+list2 = [828, 71, 92, 682, 38, 7, 45]
+list2.sort()
+list2.reverse()
+
+
+# list in list
+list3 = ["p", "y", "t", "h", ["q", "u"], "o", "n"]
+
+a = list3[4]
+b = list3[4][1]
+
+
+
+
+print(b)
+
+
+
+
+
