a70fbd59294b5e244bd2d8dad27f35858d5c8aeb
woosa7 <woosa7@gmail.com>
2016-10-18 17:47:47 +0900

    Pandas

--- KMU/DataCamp03.py
+++ KMU/DataCamp03.py
@@ -36,8 +36,41 @@ print(europe)
 # Pandas
 # --------------------------------------------
 
+import pandas as pd
 
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
 
+my_dict = {
+    'country':names,
+    'drives_right':dr,
+    'cars_per_cap':cpc
+}
+
+# Build a DataFrame
+cars = pd.DataFrame(my_dict)
+cars.index = ['US', 'AUS', 'JAP', 'IN', 'RU', 'MOR', 'EG']
+
+print(cars)
+
+# data import from csv
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+print(cars)
+
+print(cars["country"])      # output as Pandas Series
+
+print(cars[["country"]])    # output as Pandas DataFrame
+print(cars[["country", "drives_right"]])
+
+# loc & iloc
+print(cars.loc[['RU', 'AUS']])
+print(cars.iloc[[4, 1]])
+
+print(cars.loc['MOR', 'drives_right'])
+
+print(cars.loc[['RU','MOR'], ['country', 'drives_right']])  # sub-DataFrame
 
 
 
---
+++ KMU/DataCamp04.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# data import from csv
+cars = pd.read_csv('cars.csv', index_col = 0)
+
